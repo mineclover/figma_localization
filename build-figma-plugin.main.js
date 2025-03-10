b676d09c7284b1de5f093bea5887883af63a6165
mineclover <mineclover@naver.com>
2025-03-10 17:14:13 +0900

    빌드 에러 수정

--- build-figma-plugin.main.js
+++ build-figma-plugin.main.js
@@ -3,5 +3,9 @@ module.exports = function (buildOptions) {
 	return {
 		...buildOptions,
 		// ...
+		alias: {
+			'!../css/base.css': './node_modules/@create-figma-plugin/ui/lib/css/base.css',
+			strnum: './node_modules/strnum/strnum.js',
+		},
 	}
 }
